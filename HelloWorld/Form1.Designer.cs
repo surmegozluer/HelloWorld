2df0181a084d7bfde4ca66f654e0ce472d7144c4
surmegozluer <surmegozluer@SODEC-IS-PC>
2016-08-11 23:43:11 +0300

    HelloWorld Projesi Hayırlı olsun.. :)

--- HelloWorld/Form1.Designer.cs
+++ HelloWorld/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace HelloWorld
 {
-    partial class Form1
+    partial class FrmHelloWorld
     {
         /// <summary>
         /// Required designer variable.
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FrmHelloWorld
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "FrmHelloWorld";
+            this.Text = "Hello World";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace HelloWorld
@@ -16,7 +13,7 @@ namespace HelloWorld
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FrmHelloWorld());
         }
     }
 }
